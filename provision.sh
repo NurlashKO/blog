1d4a025ed084257072842bdb82d7658c3452213b
NurlashKO <nurlashko@mercari.com>
2022-05-19 19:09:58 +0900

    Add provisioning script

---
+++ provision.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DOCKER_ARGS() {
+  local name="$1";
+  echo "-d --rm --network internal --hostname ${name} --name ${name}"
+}
+
+# Cleanup everything
+docker kill $(docker ps -q)
+docker system prune -af
+
+# Setup networking
+docker network create internal
+
+# Deploy container watcher
+docker run $(DOCKER_ARGS watcher) \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    containrrr/watchtower --revive-stopped --interval 5
+
+# Deploy microservices
+docker run $(DOCKER_ARGS loadbalancer) \
+    -p 80:80 \
+    gcr.io/kouzoh-p-nurlashko/nurlashko/nginx
+docker run $(DOCKER_ARGS blog) \
+    crccheck/hello-world
