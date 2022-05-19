ed1904b2d092c7683690aa666e2a2a5b3df1c417
NurlashKO <nurlashko@mercari.com>
2022-05-19 20:04:32 +0900

    Add reload to nginx

--- provision.sh
+++ provision.sh
@@ -13,16 +13,12 @@ docker system prune -af
 docker network create internal
 
 # Deploy container watcher
-docker run $(DOCKER_ARGS watcher) \
+docker run $(DOCKER_DEFAULT_ARGS watcher) \
     -v /var/run/docker.sock:/var/run/docker.sock \
     containrrr/watchtower --revive-stopped --interval 5
 
 # Deploy microservices
-docker run $(DOCKER_ARGS loadbalancer) \
+docker run $(DOCKER_DEFAULT_ARGS loadbalancer) \
     -p 80:80 \
     -p 443:443 \
-    --entrypoint /bin/sh -c 'while :; do sleep 6h & wait $${!}; nginx -s reload; done & nginx -g \"daemon off;\"' \
     gcr.io/kouzoh-p-nurlashko/nurlashko/nginx
-
-docker run $(DOCKER_ARGS blog) \
-    crccheck/hello-world
