3feaff42eba736a0d4fd482d9f3ee8011bdc3681
NurlashKO <nurlashko@mercari.com>
2022-05-20 23:06:17 +0900

    kinda working

--- provisioner/init-infra.sh
+++ provisioner/init-infra.sh
@@ -20,4 +20,5 @@ WantedBy=timers.target
 EOM
 
 docker pull gcr.io/kouzoh-p-nurlashko/nurlashko/provisioner
+docker run -d -v /home/nurlashko/provision:/opts/provision/tmp
 docker run -d -v /home/nurlashko/provision:/opts/provision gcr.io/kouzoh-p-nurlashko/nurlashko/provisioner
