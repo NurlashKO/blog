187ad7874fff0654fd60b6cd08de256d0ae4509d
NurlashKO <nurlashko@mercari.com>
2022-05-20 22:43:41 +0900

    Another experiment

---
+++ provisioner/init-infra.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cat > /etc/systemd/system/blog.service <<- EOM
+[Unit]
+Description=Provision infrastructure
+[Service]
+ExecStart=/bin/sh /home/nurlashko/provision.sh
+ExecPost=/bin/rm -rf /home/nurlashko/provision.sh
+EOM
+
+cat > /etc/systemd/system/blog.timer <<- EOM
+[Unit]
+Description=Check and provision if needed every 10 seconds
+[Timer]
+OnBootSec=10
+OnUnitActiveSec=10
+AccuracySec=1ms
+[Install]
+WantedBy=timers.target
+EOM
+
+docker run -v /home/nurlashko/provision.sh:/provision.sh gcr.io/kouzoh-p-nurlashko/nurlashko/provisioner
