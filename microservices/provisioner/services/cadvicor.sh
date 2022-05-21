eeed14ea359b0356119e8c2cd2c753800e1ecb20
NurlashKO <nurlashko@mercari.com>
2022-05-22 02:14:37 +0900

    Add prometheus and cadvicor deployments

---
+++ microservices/provisioner/services/cadvicor.sh
@@ -0,0 +1,9 @@
+docker run $(DOCKER_DEFAULT_ARGS cadvicor) \
+    --volume=/:/rootfs:ro \
+    --volume=/var/run:/var/run:ro \
+    --volume=/sys:/sys:ro \
+    --volume=/var/lib/docker/:/var/lib/docker:ro \
+    --volume=/dev/disk/:/dev/disk:ro \
+    --privileged \
+    --device=/dev/kmsg \
+  gcr.io/kouzoh-p-nurlashko/nurlashko/cadvicor
---
+++ microservices/provisioner/services/prometheus.sh
@@ -0,0 +1,2 @@
+docker run $(DOCKER_DEFAULT_ARGS prometheus) \
+    gcr.io/kouzoh-p-nurlashko/nurlashko/prometheus
