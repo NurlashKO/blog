aef545aa0cf9d7a787996938029af25f4941d7bf
NurlashKO <nurlashko@mercari.com>
2022-05-22 01:08:36 +0900

    Fix typo

--- microservices/provisioner/services/ingress.sh
+++ microservices/provisioner/services/ingress.sh
@@ -1,6 +1,6 @@
 mkdir -p ./data/certificates
 
-docker run $(DOCKER_DEFAULT_ARGS ingress)
+docker run $(DOCKER_DEFAULT_ARGS ingress) \
     -p 80:80 \
     -p 443:443 \
     -v $(pwd)/data/certificates:/etc/letsencrypt \
