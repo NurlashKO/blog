4c53126c027e6cb3e4dd7ec21a983bbbead82a13
NurlashKO <nurlashko@mercari.com>
2022-05-21 23:30:42 +0900

    More logs

--- microservices/provisioner/run.sh
+++ microservices/provisioner/run.sh
@@ -31,6 +31,9 @@ docker run $(DOCKER_DEFAULT_ARGS loki) \
 
 docker run $(DOCKER_DEFAULT_ARGS promtail) \
     -v /var/lib/docker/:/var/lib/docker:ro \
+    -v /var/log/journal/:/var/log/journal/:ro \
+    -v /run/log/journal/:/run/log/journal/:ro \
+    -v /etc/machine-id:/etc/machine-id:ro \
     gcr.io/kouzoh-p-nurlashko/nurlashko/promtail
 
 mkdir -p ./data/certificates && \
