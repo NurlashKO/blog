afc94798d4001a50030123d228458eb3d4af913a
NurlashKO <nurlashko@mercari.com>
2022-05-23 01:10:38 +0900

    Make better hello world

--- microservices/blog/main.go
+++ microservices/blog/main.go
@@ -1,13 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	r.LoadHTMLGlob("/templates/index.tmpl")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{})
+		//c.JSON(200, gin.H{
+		//	"message": "Hello World!",
+		//	"source code": "https://github.com/NurlashKO/blog",
+		//	"docs": "https://docs.nurlashko.de",
+		//	"grafana": "https://grafana.nurlashko.de",
+		//	"prometheus": "https://prometheus.nurlashko.de",
+		//})
 	})
 	r.Run(":8000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
