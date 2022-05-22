f7adab02323811fd008600c41f4bf521636391b4
NurlashKO <nurlashko@mercari.com>
2022-05-22 17:39:20 +0900

    conf node_collector

--- microservices/provisioner/services/node-exporter.sh
+++ microservices/provisioner/services/node-exporter.sh
@@ -3,4 +3,11 @@ docker run $(DOCKER_DEFAULT_ARGS node-exporter) \
     -v /proc:/host/proc:ro \
     -v /sys:/host/sys:ro \
     -v /:/rootfs:ro \
-    gcr.io/kouzoh-p-nurlashko/nurlashko/node-exporter --collector.systemd --collector.processes
+    -v /run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket:ro \
+    gcr.io/kouzoh-p-nurlashko/nurlashko/node-exporter \
+     --path.procfs=/host/proc \
+     --path.sysfs=/host/sys \
+     --path.rootfs=/rootfs \
+     --collector.filesystem.ignored-mount-points='^/(sys|proc|dev|host|etc)($$|/)' \
+     --collector.systemd \
+     --collector.processes
