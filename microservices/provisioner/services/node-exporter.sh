c6826018eb973a956d4bb38535452e5312f96d39
NurlashKO <nurlashko@mercari.com>
2022-05-22 17:20:46 +0900

    Decrease scrape value

--- microservices/provisioner/services/node-exporter.sh
+++ microservices/provisioner/services/node-exporter.sh
@@ -3,4 +3,4 @@ docker run $(DOCKER_DEFAULT_ARGS node-exporter) \
     -v /proc:/host/proc:ro \
     -v /sys:/host/sys:ro \
     -v /:/rootfs:ro \
-    gcr.io/kouzoh-p-nurlashko/nurlashko/node-exporter
+    gcr.io/kouzoh-p-nurlashko/nurlashko/node-exporter --collector.systemd --collector.processes
