0fe1445de9f9a6353f3f2c6d52ca915e78815bcd
NurlashKO <nurlashko@mercari.com>
2022-05-22 16:39:17 +0900

    Provisioning of node exporter

---
+++ microservices/provisioner/services/node-exporter.sh
@@ -0,0 +1,6 @@
+
+docker run $(DOCKER_DEFAULT_ARGS node-exporter) \
+    -v /proc:/host/proc:ro \
+    -v /sys:/host/sys:ro \
+    -v /:/rootfs:ro \
+    gcr.io/kouzoh-p-nurlashko/nurlashko/node-exporter
