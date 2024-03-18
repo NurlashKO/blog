3511c59e77e94cd47f4ede8fe0d722c8114be83f
nurlashko <nurlashko@mercari.com>
2024-03-18 10:56:48 +0900

    Add database service

--- microservices/provisioner/services/database.sh
+++ microservices/provisioner/services/database.sh
@@ -2,4 +2,5 @@ docker run $(DOCKER_DEFAULT_ARGS database) \
     -v "$(pwd)/data/pgdata:/var/lib/postgresql/data" \
     -e POSTGRES_PASSWORD=tmp \
     -e POSTGRES_USER=nurlashko \
+    -p 5432:5432 \
     gcr.io/kouzoh-p-nurlashko/nurlashko/database
