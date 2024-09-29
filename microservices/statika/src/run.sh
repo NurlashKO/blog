78a4a8fb418e02eb453647da97d8c54220c47ea4
nurlashko <nurlashko@mercari.com>
2024-09-29 12:38:03 +0900

    Trying to move statika to header auth

--- microservices/statika/src/run.sh
+++ microservices/statika/src/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/filebrowser -c /config/settings.json config set --auth.method=proxy --auth.header=X-AUTH-STATIKA
+/filebrowser config set --auth.method=proxy --auth.header=X-AUTH-STATIKA
 /filebrowser
