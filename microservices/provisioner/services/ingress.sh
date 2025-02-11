2c2cf3bfc20815e67bc933986c23d003a3633ac4
nurlashko <nurlashko@mercari.com>
2025-02-11 18:19:55 +0900

    [main] Close 1194 in ingress and move to vpn

--- microservices/provisioner/services/ingress.sh
+++ microservices/provisioner/services/ingress.sh
@@ -4,7 +4,6 @@ docker run $(DOCKER_DEFAULT_ARGS ingress) \
     -p 80:80 \
     -p 443:443 \
     -p 943:943 \
-    -p 1194:1194 \
     -v $(pwd)/data/certificates:/etc/letsencrypt \
     -v $(pwd)/data/statika/srv/images:/www/data/images \
     gcr.io/kouzoh-p-nurlashko/nurlashko/ingress
