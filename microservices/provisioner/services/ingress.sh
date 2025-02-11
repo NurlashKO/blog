e9affca2f1c29189b9e14f52e2f2bea1d1cbbd3b
nurlashko <nurlashko@mercari.com>
2025-02-11 17:57:24 +0900

    [main] Open 943 port

--- microservices/provisioner/services/ingress.sh
+++ microservices/provisioner/services/ingress.sh
@@ -3,6 +3,7 @@ mkdir -p ./data/certificates
 docker run $(DOCKER_DEFAULT_ARGS ingress) \
     -p 80:80 \
     -p 443:443 \
+    -p 943:943 \
     -p 1194:1194 \
     -v $(pwd)/data/certificates:/etc/letsencrypt \
     -v $(pwd)/data/statika/srv/images:/www/data/images \
