b5ebb773934cf6cd951f08197567fa05f470e01e
nurlashko <nurlashko@mercari.com>
2025-02-11 15:52:49 +0900

    [main] Attempt to setup vpn

--- microservices/provisioner/services/vpn.sh
+++ microservices/provisioner/services/vpn.sh
@@ -1,2 +1,7 @@
 docker run $(DOCKER_DEFAULT_ARGS vpn) \
+  --device /dev/net/tun  \
+  --cap-add=MKNOD \
+  --cap-add=NET_ADMIN \
+  -p 1194:1194/udp \
+  -v "$(pwd)/credentials/vpn:/openvpn" \
     gcr.io/kouzoh-p-nurlashko/nurlashko/vpn
--- microservices/vpn/src/run.sh
+++
@@ -1,3 +0,0 @@
-echo "HELLO WORLD"
-
-sleep 10000
